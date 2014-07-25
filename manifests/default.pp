f43c1801a690d8b1d57607582c89a44006d56ae9
Alastair Lynn <arplynn@gmail.com>
2014-07-25 16:48:41 +0100

    Add the screens system

--- manifests/default.pp
+++ manifests/default.pp
@@ -19,12 +19,33 @@ node "srcomp" {
         managehome => true
     }
 
-    vcsrepo { "/home/competition/test":
+    vcsrepo { "/home/competition/srcomp-http":
         ensure   => present,
-        source   => "${git_root}/test.git",
+        source   => "${git_root}/comp/srcomp-http.git",
         user     => 'competition',
         revision => 'master',
         require  => User['competition']
+    }
+
+    vcsrepo { "/home/competition/srcomp-screens":
+        ensure   => present,
+        source   => "${git_root}/comp/srcomp-screens.git",
+        user     => 'competition',
+        revision => 'master',
+        require  => User['competition']
+    }
+
+    package { ['python-flask',
+               'python-dateutil',
+               'python-nose',
+               'python-yaml',
+               'python-simplejson']:
+        ensure => latest,
+        before => Notify['ready']
+    }
+
+    notify { "ready":
+        message => "Ready to run"
     }
 }
 
